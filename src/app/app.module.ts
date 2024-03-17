b7d4cad5ffc8156a46ad8e0613609af9a5cbc724
dimonsay <dimonwar12@gmail.com>
2024-03-17 02:44:50 +0200

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { AboutusComponent } from './components/homepage/aboutus/aboutus.component';
+import { DatingComponent } from './components/homepage/dating/dating.component';
+import { FaqComponent } from './components/homepage/faq/faq.component';
+import { SignUpComponent } from './components/homepage/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomepageComponent,
+    AboutusComponent,
+    DatingComponent,
+    FaqComponent,
+    SignUpComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/homepage/dating/dating.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dating',
+  templateUrl: './dating.component.html',
+  styleUrl: './dating.component.css'
+})
+export class DatingComponent {
+
+}
